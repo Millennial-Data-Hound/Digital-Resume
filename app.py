dc0cb2b1e087c1ce3abd19a9ec24aa08569c9a51
Millennial-Data-Hound <larrysimmons1@live.com>
2024-06-10 12:47:33 -0400

    Change 11

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ Senior Data Analyst, assisting enterprises by supporting data-driven decision-ma
 EMAIL = "larrysimmons1@live.com"
 SOCIAL_MEDIA = {
     "YouTube": "https://youtube.com/c/codingisfun",
-    "LinkedIn": "https://linkedin.com/in/stephensimmonsda.com",
+    "LinkedIn": "https://linkedin.com/in/stephensimmonsda/",
     "GitHub": "https://github.com/MrSimmons2u/Stephen_Simmons_Repository",
     "Twitter": "https://twitter.com",
 }
